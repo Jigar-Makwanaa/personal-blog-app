5064268ebf5568b5eeed53893032dc9940a3088b
jigar makwana <jigarmakwana34065@gmail.com>
2024-09-17 23:06:32 +0530

    re again setup react task

---
+++ src/component/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="main">
+                            <div className="menu">
+                                <div className="setting">
+                                    <span>
+                                    <i class="fa-solid fa-gear"></i>
+                                    </span>
+                                </div>
+                                <div className="discover">
+
+                                </div>
+                                <div className="myfeed">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
